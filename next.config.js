48edcbcb7d52a4450264007f09e12eec534a7b87
Kübra Çınar İlmen <kubracinar1991@gmail.com>
2022-04-28 12:39:50 +0300

    style eklendi

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
-module.exports = {
-  env:{
-    apiKey: process.env.API_KEY,
-  },
-  serverRuntimeConfig:{
-    apiKey: process.env.API_KEY,
-  },
+// require('dotenv').config()
 
+module.exports = {
+  env: {
+    apiKey: process.env.API_KEY,
+  },
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    apiKey: process.env.API_KEY, // Pass through env variables
+  },
   publicRuntimeConfig: {
     // Will be available on both server and client
     apiKey: process.env.API_KEY,
